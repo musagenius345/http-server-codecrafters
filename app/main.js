92b074597f82ccdade5163181875d02634bce808
musagenius345 <musabadru@gmail.com>
2023-10-09 13:13:39 +0300

    test

--- app/main.js
+++ app/main.js
@@ -5,6 +5,12 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  socket.on("data", (data) => {
+
+    let successResponse = 'HTTP/1.1 200 OK\r\n\r\n' 
+    socket.write(successResponse, "utf-8")
+  })
+
   socket.on("close", () => {
     socket.end();
     server.close();
