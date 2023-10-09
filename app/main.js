f2309942123186cfa0997e498d2238d7255d9700
musagenius345 <musabadru@gmail.com>
2023-10-09 13:38:26 +0300

    test

--- app/main.js
+++ app/main.js
@@ -2,13 +2,13 @@ const net = require("net");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
-
+const CLRF = '\r\n\r\n'
 // GET /index.html HTTP/1.1
 //
 // Host: localhost:4221
 // User-Agent: curl/7.64.1
 function parseRequest(req){
-  const [startLine, ...headers] = req.split('\r\n\r\n')
+  const [startLine, ...headers] = req.split(CLRF)
   const path = startLine[1]
   return path
 }
@@ -16,9 +16,14 @@ function parseRequest(req){
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-
-    let successResponse = 'HTTP/1.1 200 OK\r\n\r\n' 
-    socket.write(successResponse, "utf-8", () => {
+    const path = parseRequest(data)
+    let response
+    if(path.startsWith('/')){
+      response= `HTTP/1.1 200 OK${CLRF}`
+    } else {
+      response = `HTTP/1.1 400 Not Found${CLRF}`
+    }
+    socket.write(response, "utf-8", () => {
       console.log('Response sent to client')
       socket.end('close', () => {
         console.log('Server disconnnected')
