d6e15576db4add06f5047e9448f142d733ac4555
musagenius345 <musabadru@gmail.com>
2023-10-09 18:53:08 +0300

    test

--- app/main.js
+++ app/main.js
@@ -14,12 +14,10 @@ function parseRequest(req) {
   const responseBody = headers[headers.length - 1]
   const headerObject = Object.fromEntries(headers.map(line => line.split(': ')));
   const userAgent = headerObject['User-Agent']
-  // console.log(headers)
   const [method, rawPath, version] = startLine.split(' ')
   return [ method,rawPath, userAgent, responseBody]
 }
 
-// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
 
@@ -29,23 +27,18 @@ const server = net.createServer((socket) => {
     const userAgentEndPoint = rawPath === '/user-agent'
     let response;
 
-    // console.log(`Path: ${rawPath}`);
 
     if (rawPath === '/') {
       response = `HTTP/1.1 200 OK${CRLF}`;
     } else if (echoEndPoint) {
       const randomString = rawPath.replace(/^\/echo\//, '');
-      // console.log('Random String: ', randomString);
       response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${randomString.length}${CRLF}${randomString}`;
     } else if (userAgentEndPoint) {
       response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}${CRLF}${userAgent}`
     } else if (method === 'GET' && filesEndPoint) {
-      // console.log(`method: ${method}\n path: ${path}`)
       const filename = rawPath.replace(/^\/files\//, '')
       const directory = process.argv[3]
       const filePath = path.join(directory, filename)
-      // console.log('filePath: ', filePath)
-      // console.log(`directory: ${directory}\nfile name: ${filename}`)
       if (fs.existsSync(filePath)) {
         const fileContent = fs.readFileSync(filePath);
         response = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fileContent.length}\r\n\r\n${fileContent}`;
@@ -53,13 +46,10 @@ const server = net.createServer((socket) => {
         response = `HTTP/1.1 404 Not Found${CRLF}`;
       }
     } else if (method === 'POST' && filesEndPoint) {
-      console.log(`method: ${method}\n path: ${path}`)
       const filename = rawPath.replace(/^\/files\//, '')
       const directory = process.argv[3]
       const filePath = path.join(directory, filename)
-      //const fileContent = responseBody;
       
-      console.log(`filePath: ${filePath}\nfileContent: ${responseBody}\ndirectory:${directory}`)
       fs.writeFileSync(filePath, responseBody);
       response = 'HTTP/1.1 201 CREATED\r\n\r\n';
 
