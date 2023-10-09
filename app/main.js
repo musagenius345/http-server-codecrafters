db94ffa32531a3da01ef7b0e54698eaa2a6662c7
musagenius345 <musabadru@gmail.com>
2023-10-09 13:33:48 +0300

    test

--- app/main.js
+++ app/main.js
@@ -3,6 +3,16 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
+// GET /index.html HTTP/1.1
+//
+// Host: localhost:4221
+// User-Agent: curl/7.64.1
+function parseRequest(req){
+  const [startLine, ...headers] = req.split('\r\n\r\n')
+  const path = startLine[1]
+  return path
+}
+
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
